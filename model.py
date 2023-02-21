16882d3af3eb4ceafe0102d14e4118fd6dc3f25d
Dennis Grötzinger <36011584+Eoli-an@users.noreply.github.com>
2023-02-21 11:35:44 +0100

    Bugfix

--- model.py
+++ model.py
@@ -121,7 +121,7 @@ class GPTConfig:
     n_embd: int = 768
     dropout: float = 0.0
     bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
-    n_simulated_layer = 12
+    n_simulated_layer: = 12
 
 class GPT(nn.Module):
 
