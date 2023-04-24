07be973b76a4aaab685f8f69cbe815348821bd31
Dennis <dennis.groetzinger@web.de>
2023-04-24 17:37:50 +0200

    0.7

--- model.py
+++ model.py
@@ -174,7 +174,7 @@ class GPT(nn.Module):
         elif isinstance(module, nn.Embedding):
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
-    def forward(self, idx, targets=None, confidence_threshold=0.05):
+    def forward(self, idx, targets=None, confidence_threshold=0.7):
         device = idx.device
         b, t = idx.size()
         assert t <= self.config.block_size, f"Cannot forward sequence of length {t}, block size is only {self.config.block_size}"
